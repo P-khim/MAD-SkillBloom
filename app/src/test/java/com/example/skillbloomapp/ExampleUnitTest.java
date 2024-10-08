a71217de684c35e42614c69d8d239057995ab17f
Neth Wandara <ytdare51@gmail.com>
2024-10-08 18:25:36 +0700

    Push Demmy Code

--- app/src/test/java/com/example/skillbloomapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/skillbloomapp/ExampleUnitTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public static void main(String[] args) {
+        System.out.println("This is testing 101");
     }
 }
